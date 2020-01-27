db4bd6bb903093a8bc3bcb50d9c23e7568e5b38f
tdingquan <tdingquan@gmail.com>
2020-01-27 23:47:14 +0800

    init database tables

--- calorie/calorie/settings.py
+++ calorie/calorie/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dish',
+    'menu',
+    'searchitem',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = None
---
+++ calorie/dish/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+
+class Dish(models.Model):
+    '''菜品'''
+
+    picture = models.FileField(_("dish picture"), upload_to=None, max_length=100)
+    name = models.CharField(_("dish name"), max_length=50)
+    calorie = models.IntegerField(_("dish calorie"))
+    like = models.IntegerField(_("dish like amount"))
+    dislike = models.IntegerField(_("dish dislike amount"))
+    tag = models.ManyToManyField("Tag", verbose_name=_("tag"))
+
+    class Meta:
+        verbose_name = _("Dish")
+        verbose_name_plural = _("Dishes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Dish_detail", kwargs={"pk": self.pk})
+
+
+class Tag(models.Model):
+    '''菜品标签'''
+
+    name = models.CharField(_("tag"), max_length=50)
+
+    class Meta:
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Tag_detail", kwargs={"pk": self.pk})
