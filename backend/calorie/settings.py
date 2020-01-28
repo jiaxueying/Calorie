c2884687021c3621b4ef220b0e3bc70362777a0d
He Bo <nku_hb@outlook.com>
2020-01-28 10:34:13 +0800

    Fix bug at CORS_ORIGIN_WHITELIST in settings.

--- backend/calorie/settings.py
+++ backend/calorie/settings.py
@@ -141,9 +141,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    '*'
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1',
+    'https://127.0.0.1',
+]
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
