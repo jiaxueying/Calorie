6d988b6b86b0490ff69174c83f869c12c948c752
He Bo <nku_hb@outlook.com>
2020-01-30 18:02:10 +0800

    Add some global Class into calorie.api

--- backend/searchitem/views.py
+++ backend/searchitem/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-
-from rest_framework.views import APIView, Response
+from rest_framework.views import APIView as RestfulAPIView
+from calorie.api import APIView
+from rest_framework.views import Response
 from rest_framework import status
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.parsers import FileUploadParser
 from rest_framework.authtoken.models import Token
-from rest_framework.exceptions import APIException, ParseError
+from rest_framework.exceptions import APIException
+from rest_framework.exceptions import ParseError
 from django.db.models.query import QuerySet
 
 from searchitem.models import HistorySearch
@@ -16,19 +20,11 @@ from django.db.models import Count
 
 from searchitem.serializers import SearchItemSerializer
 
+from calorie.api import FieldException
+from calorie.api import NotImplementedExecption
+from calorie.api import NoAttributeInDatabaseException
 # Create your views here.
 
-class NotImplementedExecption(APIException):
-    status = status.HTTP_501_NOT_IMPLEMENTED
-    defualt_detail = '尚未实现该接口'
-
-class FieldException(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = '该请求需要字段'
-
-class NoAttributeInDatabase(APIException):
-    status_code = status.HTTP_501_NOT_IMPLEMENTED
-    default_detail = 'This attribute is not in database.'
 
 def get_int(input_data, field_name):
     try:
@@ -50,14 +46,12 @@ def check_and_get_int(request_data, field_name):
         temp_data = temp_data[0]
     return get_int(temp_data, field_name)
     
-
-
 class GetSearchHistoryAPI(APIView):
     def get(self, request):
         user_id = self.get_user_id(request)
         popular_items = self.get_popular_item()
         history_items = self.get_history_item(user_id)
-        return Response(data={'history_items': history_items, 'popular_items': popular_items}, status=status.HTTP_200_OK)
+        return self.success(data={'history_items': history_items, 'popular_items': popular_items})
     
     def get_user_id(self, request):
         request_data = request.query_params
