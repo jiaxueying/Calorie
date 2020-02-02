59fb4e9832abee470fc62af52b29da8a28efa283
hanlinghao <417564526@qq.com>
2020-02-02 20:56:25 +0800

    refine code style of .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,43 +3,43 @@
 module.exports = {
   root: true,
   parserOptions: {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     browser: false,
     node: true,
-    es6: true
+    es6: true,
   },
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
   ],
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/strongly-recommended',
-    'standard'
+    'standard',
   ],
 
   // add your custom rules here
-  'rules': {
-    'eqeqeq': ['error', 'smart'],
-    "space-infix-ops": ["error"],
-    'semi': ['error', 'always'],
-    "no-extra-semi": "error",
-    'brace-style': ['error', "1tbs", {
-      "allowSingleLine": true
+  rules: {
+    eqeqeq: ['error', 'smart'],
+    'space-infix-ops': ['error'],
+    semi: ['error', 'always'],
+    'no-extra-semi': 'error',
+    'brace-style': ['error', '1tbs', {
+      allowSingleLine: true,
     }],
     'object-curly-spacing': 0,
-    "space-before-function-paren": ["error", "never"],
-    "space-before-blocks": ["error", "always"],
-    "keyword-spacing": "error",
-    "space-in-parens": ["error", "never"],
-    "comma-dangle": ["error", "always-multiline"],
+    'space-before-function-paren': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'keyword-spacing': 'error',
+    'space-in-parens': ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
@@ -48,26 +48,26 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 
     'vue/no-parsing-error': ['error', {
-      'x-invalid-end-tag': false
+      'x-invalid-end-tag': false,
     }],
     'vue/max-attributes-per-line': [2, {
-      'singleline': 1,
-      'multiline': {
-        'max': 1,
-        'allowFirstLine': false
-      }
+      singleline: 1,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
     }],
     'vue/html-indent': ['error', 2, {
-      'attribute': 1,
-      'closeBracket': 0,
-      'alignAttributesVertically': true,
-      'ignores': []
+      attribute: 1,
+      closeBracket: 0,
+      alignAttributesVertically: true,
+      ignores: [],
     }],
     'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'always'
+      singleline: 'never',
+      multiline: 'always',
     }],
-    "vue/html-quotes": ["error", "double"]
+    'vue/html-quotes': ['error', 'double'],
   },
-  "globals": {}
-}
+  globals: {},
+};
