b6f21ad989bfef71e7a7613c79308ac91efef3fb
hanlinghao <417564526@qq.com>
2020-02-02 20:51:10 +0800

    finish git hook

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,14 +16,13 @@ module.exports = {
   },
   // required to lint *.vue files
   plugins: [
-    'vue',
-    'html'
+    'vue'
   ],
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/strongly-recommended',
-    'eslint:recommended'
+    'standard'
   ],
 
   // add your custom rules here
--- frontend/init.sh
+++ frontend/init.sh
@@ -21,7 +21,7 @@ fi
 echo -e "\033[32mInstalling Vue.js\033[0m\n"
 cnpm install -g @vue/cli @vue/cli-init
 echo -e "\033[32mInstalling ESLint\033[0m\n"
-cnpm install -g babel-eslint eslint eslint-config-standard eslint-friendly-formatter eslint-loader eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard eslint-plugin-vue eslint-plugin-html
+cnpm install -g eslint@5.16.0 eslint-plugin-vue@5.2.3 eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
 echo -e "\033[32mInstalling stylelint\033[0m\n"
 cnpm install -g stylelint stylelint-config-standard stylelint-order stylelint-processor-html stylelint-webpack-plugin
 echo -e "\033[32mInstalling frontend test tool\033[0m\n"
