223a58454a0dd8943c7af7e0ee49d6d4c32ed735
He Bo <nku_hb@outlook.com>
2020-02-05 00:06:47 +0800

    Set media url in calorie.urls

--- backend/calorie/urls.py
+++ backend/calorie/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('searchitem/', include('searchitem.urls')),
@@ -23,3 +26,4 @@ urlpatterns = [
     path('dish/', include('dish.urls')),
     path('user/', include('user.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
