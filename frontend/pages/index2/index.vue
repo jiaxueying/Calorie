a51aa731dfd8901c90b49188a53473d5291c79be
jiaxueying <60513918+jiaxueying@users.noreply.github.com>
2020-02-04 16:15:42 +0800

    recommendation

---
+++ frontend/pages/index2/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <Meal></Meal>
+  <input type="button" @click="toPath" value="nextpage">
+</template>
+
+<script>
+  import Meal from './recommend.vue'
+   export default {
+     components: {
+      Meal
+    },
+    data() {
+      return {
+      };
+    },
+   
+    onLoad() {
+        
+    },
+    onPullDownRefresh(){
+      
+    },
+    methods: {
+      toPath(){
+        uni.switchTab({
+          url:"./recommend"
+        })
+      }
+  
+    },
+  };
+</script>
+
+<style>
+</style>
---
+++ frontend/pages/index2/recommend.vue
@@ -0,0 +1,53 @@
+<template>
+  <view class="content">
+  <view class="content1">
+    <input type="checkbox" name="check" value=" ">
+    <image style="width: 100px;height: 100px" src="../../static/logo.png"></image>
+  </view>
+  <view class="content2">
+    <view class="content2a">
+      <h2>{{meal-name}}</h2>
+    </view>
+    <view class="content2b">
+      <p>{{calorie}}</p>
+      <picker range="number"></picker>
+    </view>
+  </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: [],
+    methods: {      
+   
+     },
+     data() {
+       return {
+         'meal-name':'菜品名称',
+         calorie:'200',
+         number:['one','two','three','four']
+       };
+     },
+     
+     }
+  
+  
+</script>
+
+<style>
+  .content{
+    display:flex;
+    flex-direction: row;
+  }
+  .content1{
+    position: relative;
+    left:10%;
+    top:0px;
+    display: inline-block;
+  }
+  .content2{
+    position: relative;
+    top:18px
+  }
+</style>
