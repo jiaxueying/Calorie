3b90672e0fbf0e750cd272cc0f895974dcd0c0b8
YuKi74 <2030254130@qq.com>
2020-02-06 18:43:12 +0800

    asdfasdf

--- frontend/components/recommodation/footer.vue
+++ frontend/components/recommodation/footer.vue
@@ -1,18 +1,33 @@
 <template>
+  <view>
   <view class="footer">
     <checkbox @click="tap">全选</checkbox>
-    <button plain=true size="mini" @click="">加入菜单</button>
+    <button plain=true size="mini" @click="add">加入菜单</button>
+  </view>
+  <popup style="position: absolute;left: 0;top: 0;z-index: 3;" v-if="isshow"></popup>
   </view>
 </template>
 
 <script>
+  import popup from "./popup.vue"
   export default {
+    components:{
+      popup
+    },
+    data(){
+      return{
+        isshow:false
+      }
+    },
     props: [],
     methods: {
       tap:function(){
         this.$emit('chooseAll');
         console.log('hello')
-      }
+      },
+	  add:function(){
+		  this.isshow=true
+	  }
     }
   }
 </script>
@@ -30,6 +45,7 @@
     color: #59453D;
     background-color: #FFFFFF;
     border-top: 1px solid #59453D;
+    z-index: 2;
   }
   checkbox{
     padding-left: 8%;
