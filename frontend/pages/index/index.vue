5e537ec62f6c896865c39af087ebee906c1d7eee
YuKi74 <2030254130@qq.com>
2020-02-08 00:00:48 +0800

    1

---
+++ frontend/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view style="display: flex;flex-direction: column;align-items: center;">
+		<navigator url="indexOfRecommondation" open-type="redirect">推荐功能</navigator>
+    <navigator url="indexOfSearch" open-type="redirect">查询功能</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+navigator{
+  margin-top: 50px;
+  font-size: 30px;
+  color: #968f8c;
+}
+</style>
