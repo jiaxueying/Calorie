795497edca717b5ad7cf4a66cfcd9064fb821b5c
jiaxueying <60513918+jiaxueying@users.noreply.github.com>
2020-02-08 17:52:43 +0800

    123

--- frontend/pages/user/user.vue
+++ frontend/pages/user/user.vue
@@ -1,6 +1,13 @@
 <template>
-	<view>
-		用户中心
+	<view class="all">
+		<view class="userimg">
+			<open-data type="userAvatarUrl"></open-data>
+		</view>
+		<view class="userinfor">
+      <open-data type="userNickName" style="font-size: 2em;height:85rpx;"></open-data>
+      <text style="font-size: 1.2em;line-height: 200%;">体重：{{weight}}kg\n</text>
+      <text style="font-size: 0.7em;">本日推荐摄入卡路里范围：\n{{minCalForDay}}kcal-{{maxCalForDay}}kcal</text>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +15,9 @@
 	export default {
 		data() {
 			return {
-				
+        weight:'100',
+        minCalForDay:'1000',
+        maxCalForDay:'1500'			
 			}
 		},
 		methods: {
@@ -18,5 +27,24 @@
 </script>
 
 <style>
-
+  .all{
+    display: flex;
+  }
+	.userimg{
+    margin-top: 15%;
+		width:200rpx;
+		height:200rpx;
+		border-radius:50%;
+    overflow: hidden;
+    position:relative;
+    left:55rpx;
+	}
+  .userinfor{
+    color: #505050;
+    margin-top: 15%;
+    display: flex;
+    flex-direction: column;
+    position:relative;
+    left:100rpx
+  }
 </style>
