3c29f42bdb4607c97e271a83caf777ce77403b28
jiaxueying <1007210564@qq.com>
2022-05-22 13:27:11 +0800

    修改F用法

--- backend/dish/query.py
+++ backend/dish/query.py
@@ -1,7 +1,7 @@
 """
 查询相关函数
 """
-from django.db.models import F, FilteredRelation, Q
+from django.db.models import F, FilteredRelation, Q, ExpressionWrapper
 from django.db import transaction
 from dish.models import Dish
 from searchitem.models import SearchItem, HistorySearch
@@ -15,18 +15,18 @@ class DishQueryFunctionSet:
     def name(user_obj, keyword):
         """通过菜品部分名称查询菜品"""
         return Dish.objects.filter(name__contains=keyword).annotate(
-            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)),output_field=BooleanField() # likedish__user_id=user_obj.id
-        ).annotate(user_like=F('t__like'),output_field=BooleanField()).annotate(user_dislike=1-F('t__like'),output_field=BooleanField()).order_by('id')
+            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)) # likedish__user_id=user_obj.id
+        ).annotate(user_like=F('t__like') ).annotate(user_dislike=ExpressionWrapper(1-F('t__like'),output_field=BooleanField())).order_by('id')
 
     @staticmethod
     def tag(user_obj, keyword):
         """通过标签查询菜品"""
         return Dish.objects.prefetch_related("tag").filter(tag__name__contains=keyword).annotate(
-            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)) ,output_field=BooleanField()# likedish__user_id=user_obj.id
-        ).annotate(user_like=F('t__like'),output_field=BooleanField()).annotate(user_dislike=1-F('t__like'),output_field=BooleanField()).order_by('id')
+            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)) # likedish__user_id=user_obj.id
+        ).annotate(user_like=F('t__like')).annotate(user_dislike=ExpressionWrapper(1-F('t__like'),output_field=BooleanField())).order_by('id')
 
     @staticmethod
-    def keyword(user_obj, keyword):
+    def keyword(user_obj, keyword): 
         """通过关键字查询菜品"""
         DishQueryFunctionSet.add_search_item(user_obj, "keyword", keyword)
         return (DishQueryFunctionSet.name(user_obj, keyword) | DishQueryFunctionSet.tag(user_obj, keyword)).distinct().order_by('id')
@@ -38,16 +38,16 @@ class DishQueryFunctionSet:
             DishQueryFunctionSet.add_search_item(user_obj, "tag", str(tag_id))
 
         return Dish.objects.prefetch_related('tag').filter(tag__in=tag_list).annotate(
-            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)),output_field=BooleanField()
-        ).annotate(user_like=F('t__like'),output_field=BooleanField()).annotate(user_dislike=1-F('t__like'),output_field=BooleanField()).order_by('id')
+            t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj))
+        ).annotate(user_like=F('t__like')).annotate(user_dislike=ExpressionWrapper(1-F('t__like'),output_field=BooleanField())).order_by('id')
 
     @staticmethod
     def calorie(user_obj, min_calorie, max_calorie):
         """通过最小最大值查询"""
         return Dish.objects.filter(
             calorie__gt=min_calorie, calorie__lt=max_calorie).annotate(
-                t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj),output_field=BooleanField()) # likedish__user_id=user_obj.id
-            ).annotate(user_like=F('t__like'),output_field=BooleanField()).annotate(user_dislike=1-F('t__like'),output_field=BooleanField()).order_by('id')
+                t=FilteredRelation('likedish', condition=Q(likedish__user=user_obj)) # likedish__user_id=user_obj.id
+            ).annotate(user_like=F('t__like')).annotate(user_dislike=ExpressionWrapper(1-F('t__like'),output_field=BooleanField())).order_by('id')
 
     @staticmethod
     @transaction.atomic()
